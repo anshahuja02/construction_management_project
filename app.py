1f51ca8e118dda7d645df9f8a6e2e4065318fa33
anshahuja02 <110743389+anshahuja02@users.noreply.github.com>
2023-07-22 14:28:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+
+app = Flask(__name__)
+
+# MySQL Database Configuration
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    port=3306,
+    password="ANSH9900",
+    database="construction_project_management"
+)
+
+cursor = db.cursor()
+
+# Route for displaying all projects
+
+
+@app.route('/')
+def index():
+    cursor.execute("SELECT * FROM projects")
+    projects = cursor.fetchall()
+    return render_template('index.html', projects=projects)
+
+# Route for creating a new project
+
+
+@app.route('/create_project', methods=['GET', 'POST'])
+def create_project():
+    if request.method == 'POST':
+        name = request.form['name']
+        location = request.form['location']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        budget = float(request.form['budget'])
+        status = request.form['status']
+
+        sql = "INSERT INTO projects (name, location, start_date, end_date, budget, status) VALUES (%s, %s, %s, %s, %s, %s)"
+        cursor.execute(sql, (name, location, start_date,
+                       end_date, budget, status))
+        db.commit()
+        return redirect(url_for('index'))
+
+    return render_template('create_project.html')
+
+# Route for displaying tasks of a project
+
+
+@app.route('/project/<int:project_id>')
+def project(project_id):
+    sql = "SELECT * FROM projects WHERE id=%s"
+    cursor.execute(sql, (project_id,))
+    project = cursor.fetchone()
+
+    sql = "SELECT * FROM tasks WHERE project_id=%s"
+    cursor.execute(sql, (project_id,))
+    tasks = cursor.fetchall()
+
+    return render_template('project.html', project=project, tasks=tasks)
+
+# Route for creating a new task for a project
+
+
+@app.route('/project/<int:project_id>/create_task', methods=['GET', 'POST'])
+def create_task(project_id):
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        status = request.form['status']
+
+        sql = "INSERT INTO tasks (project_id, name, description, start_date, end_date, status) VALUES (%s, %s, %s, %s, %s, %s)"
+        cursor.execute(sql, (project_id, name, description,
+                       start_date, end_date, status))
+        db.commit()
+        return redirect(url_for('project', project_id=project_id))
+
+    return render_template('create_task.html', project_id=project_id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
